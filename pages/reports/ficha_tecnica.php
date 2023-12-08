9190b3f7935fe4f9e6641e6419c52481e3597f1a
Jose Carrizales <carrizalesj5@hotmail.com>
2023-12-08 12:05:59 -0400

    fix plantilla

---
+++ pages/reports/ficha_tecnica.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sapiente tempora dolores animi, laborum temporibus fugit officiis facere doloribus illum eligendi officia fugiat! Sapiente inventore, sunt id tempore eius voluptatibus.
+    </h1>
+</body>
+</html>
--- print_pdf.php
+++ print_pdf.php
@@ -1,9 +1,14 @@
 <?php
-    require __DIR__.'/vendor/autoload.php';
+  require __DIR__.'/vendor/autoload.php';
 
-use Spipu\Html2Pdf\Html2Pdf;
+  use Spipu\Html2Pdf\Html2Pdf;
 
-$html2pdf = new Html2Pdf();
-$html2pdf->writeHTML('<h1>Project Minaguas</h1> - El Carri, El Manaure, La Manolo, El Principe, El Gringo');
-$html2pdf->output();
+  ob_start();
+  require_once './pages/reports/ficha_tecnica.php';
+
+  $template = ob_get_clean();
+
+  $html2pdf = new Html2Pdf('P', 'A4', 'es','true','UTF-8');
+  $html2pdf->writeHTML($template);
+  $html2pdf->output('Reporte.pdf');
 ?>
