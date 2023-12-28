f2c94f25ef2226980324bd39b95718c3fc0bbdf3
Yoseli13 <yoselionel@gmail.com>
2023-12-28 10:59:57 -0400

    mantenimiento

---
+++ maintenance.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./assets/img/logos/cropped-mminaguas.webp">
+  <title>
+    Minaguas
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="./assets/js/fontawesome/42d5adcbca.js"></script>
+  <script src="./assets//js/sweetalerts.js"></script>
+  <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="./assets/css/argon-dashboard.css?v=2.0.4" rel="stylesheet" />
+</head>
+
+<body style="background-image: linear-gradient(rgb(20,28,49,0.8),rgb(20,28,49,0.8)),url(./assets/img/world-map-wallpaper.jpg);overflow-x:hidden;overflow-y:auto">
+    <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; width: 100%; height: 100vh;">
+        <h1 class='text-light font-weight-bold'>SITIO WEB EN MANTENIMIENTO</h1>
+        <img style="height: 400px; width: 400px;" src="./assets/img/socketimage2.png" />
+    </div>
+</body>
+</html>
