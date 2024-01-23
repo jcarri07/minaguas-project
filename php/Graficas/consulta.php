756645401f16927394179977d95810ef9c0df920
alfredocalderon <44610855+alfredocalderon@users.noreply.github.com>
2024-01-23 16:46:01 -0400

    Update consulta.php

--- php/Graficas/consulta.php
+++ php/Graficas/consulta.php
@@ -3,11 +3,11 @@
 include "../Conexion.php";
 $fecha_actual = date("Y");
 $f = $fecha_actual-1;
-$res = mysqli_query($conn, "SELECT * FROM datos_embalse WHERE YEAR(fecha) = '$fecha_actual' OR YEAR(fecha) = '$f';");
+$res = mysqli_query($conn, "SELECT * FROM datos_embalse WHERE YEAR(fecha) = '$fecha_actual' OR YEAR(fecha) = '$f' GROUP BY fecha DESC;");
 $count = mysqli_num_rows($res);
 if ($count >= 1) {
 
-    $re = mysqli_query($conn, "SELECT id_embalse,cota_min, cota_max FROM embalses ;");
+    $re = mysqli_query($conn, "SELECT * FROM embalses ;");
     $count = mysqli_num_rows($re);
     if ($count >= 1) {
 
