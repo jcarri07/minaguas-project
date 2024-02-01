cdce54a9714c15fab80f1475c4a8a8d9997f7393
jcarri07 <84305952+jcarri07@users.noreply.github.com>
2024-02-01 08:49:15 -0400

    abae

--- php/login/logout.php
+++ php/login/logout.php
@@ -1,9 +1,6 @@
 <?php
 
-    session_start();
-    session_unset();
-    session_destroy();
-     print "<script>window.location='../../index.php';</script>";
-    
-
-?>
+session_start();
+session_unset();
+session_destroy();
+print "<script>window.location='../../index.php';</script>";
