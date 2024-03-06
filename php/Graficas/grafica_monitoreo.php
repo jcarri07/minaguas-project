7e9f09e74103accfdf256130635e9b27014a6489
alfredocalderon <44610855+alfredocalderon@users.noreply.github.com>
2024-03-06 00:04:50 -0400

    Update grafica_monitoreo.php

--- php/Graficas/grafica_monitoreo.php
+++ php/Graficas/grafica_monitoreo.php
@@ -35,61 +35,60 @@ function calcularSemanas($fecha, $fecha2)
 
     return $semanas;
 }
-$semanas = calcularSemanas($fecha1, $fecha2);
+$semanas = calcularSemanas($fecha1, $fecha2) + $numeroSemana;
 $j = 0;
-$r = mysqli_query($conn, "SELECT DAYOFWEEK(fecha) AS dia,WEEK(fecha) semana , MAX(CONCAT(fecha, ' ', hora)) AS fecha_hora, d.id_embalse
+$i = 0;
+$r = mysqli_query($conn, "SELECT fecha,DAYOFWEEK(fecha) AS dia,WEEK(fecha) semana , MAX(CONCAT(fecha, ' ', hora)) AS fecha_hora, d.id_embalse
 FROM  datos_embalse d
 RIGHT JOIN embalses e ON e.id_embalse = d.id_embalse AND e.id_embalse = '$id'
 WHERE fecha BETWEEN '$fecha1' AND '$fecha2' AND d.estatus = 'activo' AND (DAYOFWEEK(fecha) = 2 OR ( fecha = '$fecha1' AND DAYOFWEEK('$fecha1') != 2))
-GROUP BY fecha;
-");
+GROUP BY fecha ORDER BY fecha;");
 
-$res = mysqli_query($conn, "SELECT DAYOFWEEK(fecha) AS dia,WEEK(fecha) semana , MAX(CONCAT(fecha, ' ', hora)) AS fecha_hora, d.id_embalse
+$res = mysqli_query($conn, "SELECT fecha,DAYOFWEEK(fecha) AS dia,WEEK(fecha) semana , MAX(CONCAT(fecha, ' ', hora)) AS fecha_hora, d.id_embalse
 FROM  datos_embalse d
 RIGHT JOIN embalses e ON e.id_embalse = d.id_embalse AND e.id_embalse = '$id'
 WHERE fecha BETWEEN '$fecha1' AND '$fecha2' AND d.estatus = 'activo' AND (DAYOFWEEK(fecha) = 1 OR ( fecha = '$fecha2' AND DAYOFWEEK('$fecha2') != 1))
-GROUP BY fecha;");
+GROUP BY fecha ORDER BY fecha;");
 
-$datos1 = mysqli_fetch_all($res, MYSQLI_ASSOC);
-$datos2 = mysqli_fetch_all($r, MYSQLI_ASSOC);
+$datos1 = mysqli_fetch_all($r, MYSQLI_ASSOC);
+$datos2 = mysqli_fetch_all($res, MYSQLI_ASSOC);
 $datos_json1 = json_encode($datos1);
 $datos_json2 = json_encode($datos2);
 ?>
 <script>
     console.log('<?php echo $datos_json1; ?>');
     console.log('<?php echo $datos_json2; ?>');
-    console.log('<?php echo $id; ?>');
+    console.log('<?php echo $numeroSemana; ?>');
 </script>
 <?php
 
-while ($semanas > 0) {
+while ($numeroSemana <= $semanas) {
 
-    if (isset($datos1[$j]['semana'])) {
-        if ($numeroSemana == $datos1[$j]['semana']) {
-            echo 'lunes:' . $datos1[$j]['fecha'] . '<br>';
-            $numeroSemana++;
-            $semanas--;
+    echo '<br>Semana '.$numeroSemana.':<br>';
+
+    if (isset($datos1[$i]['semana'])) {
+        if ($numeroSemana == ($datos1[$i]['semana']+1)) {
+            echo 'lunes:' . $datos1[$i]['fecha'] . '<br>';
+            $i++;
         } else {
             echo 'lunes:vacio<br>';
         }
     } else {
         echo 'lunes:vacio<br>';
     }
-    if (isset($datos1[$j]['semana'])) {
+    if (isset($datos2[$j]['semana'])) {
         if ($numeroSemana == $datos2[$j]['semana']) {
             echo 'domingo:' . $datos2[$j]['fecha'] . '<br>';
-            $numeroSemana++;
-            $semanas--;
+            $j++;
         } else {
             echo 'domingo:vacio<br>';
-            $numeroSemana++;
-            $semana--;
         }
     } else {
         echo 'domingo:vacio<br>';
-        $numeroSemana++;
-        $semanas--;
     }
-    if($semanas <0 ){$semanas =0;}
+    $numeroSemana++;
+
+
+
 };
 ?>
