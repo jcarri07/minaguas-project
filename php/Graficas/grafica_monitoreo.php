6c33570a1be647340ce6f60ea744939e84cb9fe3
alfredocalderon <44610855+alfredocalderon@users.noreply.github.com>
2024-03-05 22:23:19 -0400

    Create grafica_monitoreo.php
    En construccion

---
+++ php/Graficas/grafica_monitoreo.php
@@ -0,0 +1,95 @@
+<?php
+
+require_once '../Conexion.php';
+require_once '../batimetria.php';
+
+date_default_timezone_set("America/Caracas");
+setlocale(LC_TIME, 'es_ES.utf8');
+
+$fecha1 = date('Y-m-d', strtotime('-1 months', strtotime(date('Y-m-d'))));
+$fecha2 = date('Y');
+$fecha1 = $_GET['fecha1'];
+$fecha2 = $_GET['fecha2'];
+$id = $_GET['id'];
+$name = $_GET['name'];
+
+
+$numeroSemana = strftime('%W', strtotime($fecha1));
+function calcularSemanas($fecha, $fecha2)
+{
+    // Obtener la fecha actual y la fecha proporcionada
+    $fechaActual = new DateTime($fecha2);
+    $fechaProporcionada = new DateTime($fecha);
+
+    // Verificar que la fecha proporcionada sea anterior a la fecha actual
+    if ($fechaProporcionada >= $fechaActual) {
+        return 0; // La fecha proporcionada es igual o posterior a la fecha actual
+    }
+
+    // Ajustar la fecha actual para que sea el primer día de la semana
+    $fechaActual->modify('last monday');
+
+    // Calcular la diferencia en semanas
+    $diferencia = $fechaActual->diff($fechaProporcionada);
+    $semanas = floor($diferencia->days / 7);
+
+    return $semanas;
+}
+$semanas = calcularSemanas($fecha1, $fecha2);
+$j = 0;
+$r = mysqli_query($conn, "SELECT DAYOFWEEK(fecha) AS dia,WEEK(fecha) semana , MAX(CONCAT(fecha, ' ', hora)) AS fecha_hora, d.id_embalse
+FROM  datos_embalse d
+RIGHT JOIN embalses e ON e.id_embalse = d.id_embalse AND e.id_embalse = '$id'
+WHERE fecha BETWEEN '$fecha1' AND '$fecha2' AND d.estatus = 'activo' AND (DAYOFWEEK(fecha) = 2 OR ( fecha = '$fecha1' AND DAYOFWEEK('$fecha1') != 2))
+GROUP BY fecha;
+");
+
+$res = mysqli_query($conn, "SELECT DAYOFWEEK(fecha) AS dia,WEEK(fecha) semana , MAX(CONCAT(fecha, ' ', hora)) AS fecha_hora, d.id_embalse
+FROM  datos_embalse d
+RIGHT JOIN embalses e ON e.id_embalse = d.id_embalse AND e.id_embalse = '$id'
+WHERE fecha BETWEEN '$fecha1' AND '$fecha2' AND d.estatus = 'activo' AND (DAYOFWEEK(fecha) = 1 OR ( fecha = '$fecha2' AND DAYOFWEEK('$fecha2') != 1))
+GROUP BY fecha;");
+
+$datos1 = mysqli_fetch_all($res, MYSQLI_ASSOC);
+$datos2 = mysqli_fetch_all($r, MYSQLI_ASSOC);
+$datos_json1 = json_encode($datos1);
+$datos_json2 = json_encode($datos2);
+?>
+<script>
+    console.log('<?php echo $datos_json1; ?>');
+    console.log('<?php echo $datos_json2; ?>');
+    console.log('<?php echo $id; ?>');
+</script>
+<?php
+
+while ($semanas > 0) {
+
+    if (isset($datos1[$j]['semana'])) {
+        if ($numeroSemana == $datos1[$j]['semana']) {
+            echo 'lunes:' . $datos1[$j]['fecha'] . '<br>';
+            $numeroSemana++;
+            $semanas--;
+        } else {
+            echo 'lunes:vacio<br>';
+        }
+    } else {
+        echo 'lunes:vacio<br>';
+    }
+    if (isset($datos1[$j]['semana'])) {
+        if ($numeroSemana == $datos2[$j]['semana']) {
+            echo 'domingo:' . $datos2[$j]['fecha'] . '<br>';
+            $numeroSemana++;
+            $semanas--;
+        } else {
+            echo 'domingo:vacio<br>';
+            $numeroSemana++;
+            $semana--;
+        }
+    } else {
+        echo 'domingo:vacio<br>';
+        $numeroSemana++;
+        $semanas--;
+    }
+    if($semanas <0 ){$semanas =0;}
+};
+?>
