d7379fbac838f5397a4c2dc8169ebd5743a673c4
alfredocalderon <44610855+alfredocalderon@users.noreply.github.com>
2024-03-29 19:53:11 -0400

    Update graficas_estatus.php

--- php/Graficas/graficas_estatus.php
+++ php/Graficas/graficas_estatus.php
@@ -122,7 +122,7 @@ e.nombre_embalse, (SELECT MAX(cota_actual)
         <!--script src="../../assets/js/date-fns.js"></script-->
         <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="../../assets/js/jquery/jquery.min.js"></script>
         <script src="../../assets/js/html2canvas.min.js"></script>
         <link href="../../assets/css/style-spinner.css" rel="stylesheet" />
 
