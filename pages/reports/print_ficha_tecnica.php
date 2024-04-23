1a0450d81309e84f21e8c29b1d09e30960f1a808
jcarri07 <84305952+jcarri07@users.noreply.github.com>
2024-04-23 09:37:46 -0400

    merge

--- pages/reports/print_ficha_tecnica.php
+++ pages/reports/print_ficha_tecnica.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '../../../vendor/autoload.php';
+require __DIR__ . '/../../../vendor/autoload.php';
 
 use Spipu\Html2Pdf\Html2Pdf;
 
@@ -11,3 +11,5 @@ $template = ob_get_clean();
 $html2pdf = new Html2Pdf('P', 'A4', 'es', 'true', 'UTF-8');
 $html2pdf->writeHTML($template);
 $html2pdf->output('Reporte.pdf');
+
+?>
