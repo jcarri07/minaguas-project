b63400239d3942932376e5aadf85746be3406338
alfredocalderon <44610855+alfredocalderon@users.noreply.github.com>
2024-04-24 09:28:50 -0400

    Update grafica_dashboard.php

--- php/Graficas/grafica_dashboard.php
+++ php/Graficas/grafica_dashboard.php
@@ -234,9 +234,13 @@ if ($count >= 1) {
         });
             $("#contenedor-2").html('<?php
                                         $valor = 100 * (($volumen_fechas[1] - $volumen_fechas[2]) / $volumen_fechas[1]);
-                                        if ($valor >= 0) {
+                                        if ($valor > 0) {
 
                                             echo '<h1 class="row col-12 align-items-center"><div class="col-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" style="fill:#2dce89 !important" ><path  d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8H288c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z"/></svg></div><span class=" col-8" style="font-size:60px !important">' . round(abs($valor), 2) . '%</span></h1>';
+                                        };
+                                        if ($valor == 0) {
+
+                                            echo '<h1 class="row col-12 align-items-center"><div class="col-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg></div><span class=" col-8" style="font-size:60px !important">' . round(abs($valor), 2) . '%</span></h1>';
                                         };
                                         if ($valor < 0) {
 
@@ -246,9 +250,13 @@ if ($count >= 1) {
                                         ?>');
             $("#contenedor-3").html('<?php
                                         $valor = -100 * (($volumen_fechas[1] - $volumen_fechas[3]) / $volumen_fechas[1]);
-                                        if ($valor >= 0) {
+                                        if ($valor > 0) {
 
                                             echo '<h1 class="row col-12 align-items-center"><div class="col-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" style="fill:#2dce89 !important" ><path  d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8H288c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z"/></svg></div><span class=" col-8" style="font-size:60px !important">' . round(abs($valor), 2) . '%</span></h1>';
+                                        };
+                                        if ($valor == 0) {
+
+                                            echo '<h1 class="row col-12 align-items-center"><div class="col-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg></div><span class=" col-8" style="font-size:60px !important">' . round(abs($valor), 2) . '%</span></h1>';
                                         };
                                         if ($valor < 0) {
 
