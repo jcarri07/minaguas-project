2f65051d78de7753a79ab5db899212f8e8aaa2a0
Miguel Bencomo <mdbencomo@gmail.com>
2024-04-25 11:30:32 -0400

    formato correcto de estatus embalse

--- pages/reports/print_estatus_embalses.php
+++ pages/reports/print_estatus_embalses.php
@@ -11,5 +11,5 @@ $template = ob_get_clean();
 $html2pdf = new Html2Pdf('L', 'A4', 'es', 'true', 'UTF-8');
 $html2pdf->writeHTML($template);
 //$template = ob_end_clean();
-ob_clean();
+//ob_clean();
 $html2pdf->output('Estatus Embalses.pdf');
