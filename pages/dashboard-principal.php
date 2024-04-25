c0efa883dfd79eac0b64445d4bddb4875b7e46bc
alfredocalderon <44610855+alfredocalderon@users.noreply.github.com>
2024-04-25 01:45:17 -0400

    Update dashboard-principal.php

--- pages/dashboard-principal.php
+++ pages/dashboard-principal.php
@@ -653,7 +653,7 @@ closeConection($conn);*/
         </div>
       </footer>-->
 
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="./assets/js/Chart.js"></script>
 <!-- <script async defer src="https://buttons.github.io/buttons.js"></script> -->
 <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
 <!--script src="./assets/js/material-dashboard.min.js?v=3.0.4"></script-->
