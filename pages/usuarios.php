62eb2154d640657eb2d3a60424c91db1c08ff8a0
alfredocalderon <44610855+alfredocalderon@users.noreply.github.com>
2024-04-28 16:04:05 -0400

    Update usuarios.php

--- pages/usuarios.php
+++ pages/usuarios.php
@@ -80,7 +80,7 @@ closeConection($conn);
               </div>
               <?php if($_SESSION["Tipo"] == "SuperAdmin"){?>
               
-                <label id="" class="form-label">Ver por</label>
+                <label id="" class="form-label">Tipo</label>
                             <select name="tipo" class="form-select" required>
                                 <option value="Admin">Administrador</option>
                                 <option value="User">Usuario</option>
@@ -160,7 +160,7 @@ closeConection($conn);
 
               <?php if($_SESSION["Tipo"] == "SuperAdmin"){?>
               
-              <label id="" class="form-label">Ver por</label>
+              <label id="" class="form-label">Tipo</label>
                           <select name="Etipo" class="form-select" required>
                               <option value="Admin">Administrador</option>
                               <option value="User">Usuario</option>
