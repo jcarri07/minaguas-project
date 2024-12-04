badfc58ec05570ca0f5a8f6f9b21d681a007e601
alfredocalderon <44610855+alfredocalderon@users.noreply.github.com>
2024-12-04 13:12:47 -0400

    Update guardar-imagen.php

--- php/guardar-imagen.php
+++ php/guardar-imagen.php
@@ -3,19 +3,23 @@ session_start();
 
 $data = $_POST['imagen'];
 $i = $_POST['nombre'];
-$t = $_POST['numero'];    
+$t = $_POST['numero'];
 
-
-
-    
     $aux = $data;
-    $nombre_archivo = "imagen-" . $i . "-".$t.".png"; //nombre del archivo
-    $ruta_archivo = "../assets/img/temp/$nombre_archivo"; //ruta donde guardar el archivo
+    $nombre_archivo = "imagen-" . $i . "-" . $t . ".png"; // Nombre del archivo
+    $ruta_archivo = "../assets/img/temp/$nombre_archivo"; // Ruta donde guardar el archivo
+
+    // Decodificación de la imagen
     $aux = explode(";", $aux)[1];
     $aux = explode(",", $aux)[1];
     $aux = str_replace('data:image/png;base64,', '', $aux);
     $aux = str_replace(' ', '+', $aux);
     $imagen_bin = base64_decode($aux);
-    file_put_contents($ruta_archivo, $imagen_bin);
 
+    // Guardar la imagen en el archivo
+    if (file_put_contents($ruta_archivo, $imagen_bin) == false) {
+        echo "error en file_put_contents";
+    } else {
+        echo "si";
+    }
 ?>
