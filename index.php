c0869b854ca59851087eaa1e08e9cd831d01a687
alfredocalderon <44610855+alfredocalderon@users.noreply.github.com>
2024-12-13 11:50:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ date_default_timezone_set("America/Caracas");
 
 <head>
   <meta charset="utf-8" />
+  <meta name="referrer" content="strict-origin-when-cross-origin" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="./assets/img/logos/icon.png">
